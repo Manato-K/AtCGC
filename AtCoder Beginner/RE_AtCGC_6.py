05e318442f984df2af78e1ba397ca74a1f410b08
chrom-P <kkllmm104@gmail.com>
2021-04-05 17:02:15 +0900

    Some Sums

--- AtCoder Beginner/RE_AtCGC_6.py
+++ AtCoder Beginner/RE_AtCGC_6.py
@@ -1,1 +1,11 @@
 n, a, b = map(int, input().split())
+cnt = 0
+for i in range(n+1):
+    n = i
+    sum = 0
+    while n > 0:
+        sum += n % 10
+        n = n // 10
+        if a <= sum <= b:
+            cnt += 1
+print(cnt)
