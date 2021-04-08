5d87f132d6b200a6c01d1b77aeff3577f00d4838
chrom-P <kkllmm104@gmail.com>
2021-04-08 09:40:45 +0900

    アンバランス

--- AtCoder Beginner Contest 043/AtCGC_19.py
+++ AtCoder Beginner Contest 043/AtCGC_19.py
@@ -1,1 +1,13 @@
-s = input()
+s = list(input())
+n = len(s)
+
+ans = [-1, -1]
+if n == 2:
+    if s[0] == s[1]:
+        ans = [1, 2]
+else:
+    for i in range(n - 2):
+        if s[i] == s[i + 1] or s[i + 1] == s[i + 2] or s[i + 2] == s[i]:
+            ans = [i + 1, i + 3]
+            break
+print(ans[0], ans[1])
